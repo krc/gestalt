af1e77d9631a6d60e81f2861f915f2378a65c649
Kelly Clowers <Kelly.Clowers@gmail.com>
2012-02-01 22:58:33 -0800

    fixed up from the actually good tutorial, and tester app

--- main.py
+++ main.py
@@ -2,39 +2,36 @@ import dbus
 import dbus.service
 import dbus.glib
 import gobject
+import gtk
 from dbus.mainloop.glib import DBusGMainLoop
-
-DBusGMainLoop(set_as_default=True)
 
 class Gestalt(dbus.service.Object):
     GESTALT_INTERFACE = 'org.mpris.MediaPlayer2.gestalt'
 
-    def __init__(self, object_path):
-        dbus.service.Object.__init__(self, dbus.SessionBus(), path)
-        self._last_input = None
+    def __init__(self):
+        bus_name = dbus.service.BusName('org.mpris.MediaPlayer2.gestalt', bus=dbus.SessionBus())
+        dbus.service.Object.__init__(self, bus_name, '/org/mpris/MediaPlayer2/gestalt')
 
-    @dbus.service.method(GESTALT_INTERFACE,
-                         in_signature='v', out_signature='s')
-    def StringifyVariant(self, var):
-        self.LastInputChanged(var)      # emits the signal
-        return str(var)
+    @dbus.service.method('org.mpris.MediaPlayer2.gestalt')
+    def hello(self):
+        return "Gestalt!"
 
-    @dbus.service.signal(GESTALT_INTERFACE,
-                         signature='v')
-    def LastInputChanged(self, var):
-        # run just before the signal is actually emitted
-        # just put "pass" if nothing should happen
-        self._last_input = var
+   # def StringifyVariant(self, var):
+   #     self.LastInputChanged(var)      # emits the signal
+   #     return str(var)
 
-    @dbus.service.method(GESTALT_INTERFACE,
-                         in_signature='', out_signature='v')
-    def GetLastInput(self):
-        return self._last_input
+   # @dbus.service.signal(GESTALT_INTERFACE,
+   #                      signature='v')
+   # def LastInputChanged(self, var):
+   #     # run just before the signal is actually emitted
+   #     # just put "pass" if nothing should happen
+   #     self._last_input = var
 
+   #  @dbus.service.method(GESTALT_INTERFACE,
+   #                      in_signature='', out_signature='v')
+   # def GetLastInput(self):
+   #     return self._last_input
 
-loop = gobject.MainLoop()
-loop.run()
-
-
-
-loop.quit()
+DBusGMainLoop(set_as_default=True)
+myservice = Gestalt()
+gtk.main()
