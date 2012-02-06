3c904bac06d0de05c7a153ebf3c35990e3377948
Kelly Clowers <Kelly.Clowers@gmail.com>
2012-02-05 18:18:38 -0800

    use gobject loop instead of gtk

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import gobject
 import gtk
-from gestaltbus import GestaltBus
+import gestaltbus
 from dbus.mainloop.glib import DBusGMainLoop
+import gobject
 
 def main():
     DBusGMainLoop(set_as_default=True)
     GestaltBus()
-    gtk.main()
+    loop = gobject.MainLoop()
+    loop.run()
+
+    #gtk.main()
 
 if __name__ == '__main__':
     main()
