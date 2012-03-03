56ca4afd8f8cff14aea1d539b3c6c572d3ae96a7
Robin Clowers <robin.clowers@gmail.com>
2012-03-03 12:51:24 -0800

    fixing test

--- integration_test.py
+++ integration_test.py
@@ -5,6 +5,8 @@ import dbus
 class GestaltIntegration(unittest.TestCase):
   def testGestalt(self):
     bus = dbus.SessionBus()
-    gestalt = bus.get_object('org.mpris.MediaPlayer2.gestalt', '/org/mpris/MediaPlayer2/gestalt')
-    gestalt_hello = gestalt.get_dbus_method('hello', 'org.mpris.MediaPlayer2.gestalt')
+    gestalt = bus.get_object('org.mpris.MediaPlayer2.Gestalt',
+            '/org/mpris/MediaPlayer2/Gestalt')
+    gestalt_hello = gestalt.get_dbus_method('hello',
+            'org.mpris.MediaPlayer2')
     assert gestalt_hello() == 'Gestalt!'
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 (python main.py) &
+sleep 0.1
 python -m unittest integration_test &
 python shutdown.py
